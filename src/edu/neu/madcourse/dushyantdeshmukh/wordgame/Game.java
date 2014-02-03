978621f1cb1c582eb5e867ba4af987ae4f5ed3f0
Dushyant Deshmukh <dushyant1388@gmail.com>
2014-02-03 00:51:08 -0500

    Commit incorporating the lookup dictionary and display current score

--- src/edu/neu/madcourse/dushyantdeshmukh/wordgame/Game.java
+++ src/edu/neu/madcourse/dushyantdeshmukh/wordgame/Game.java
@@ -1,13 +1,23 @@
 package edu.neu.madcourse.dushyantdeshmukh.wordgame;
 
+import java.io.DataInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashSet;
+import java.util.Iterator;
+
 import edu.neu.madcourse.dushyantdeshmukh.R;
+import edu.neu.madcourse.dushyantdeshmukh.util.BloomFilter;
 import android.app.Activity;
+import android.content.res.AssetManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Game extends Activity implements OnClickListener {
 
@@ -26,6 +36,12 @@ public class Game extends Activity implements OnClickListener {
 
     private String currWord = "";
     private HashSet<String> currSelections = new HashSet<String>();
+    
+    private BloomFilter<String> bloomFilter;
+    private HashSet<String> wordList = new HashSet<String>();
+    
+    private boolean isCurrWordValid = false;
+    private int currScore = 0;
 
     public Game() {
         // TODO Auto-generated constructor stub
@@ -36,6 +52,7 @@ public class Game extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         int diff = getIntent().getIntExtra(KEY_DIFFICULTY, DIFFICULTY_EASY);
+        loadBitsetFromFile("compressedWordlist.txt");
         board = getInitialBoard(diff);
         // calculateUsedTiles();
         // boardView = new BoardView(this);
@@ -72,6 +89,18 @@ public class Game extends Activity implements OnClickListener {
             break;
         case R.id.wordgame_currword_button:
             String currWord = ((Button) v).getText().toString();
+            if (isCurrWordValid) {
+                //checkWord(currWord);
+                addWord(currWord);
+                ((Button) v).setText("");
+                this.currWord = "";
+                currScore += currWord.length();
+                TextView currScoreView = (TextView) findViewById(R.id.wordgame_currscore);
+                currScoreView.setText("Score: " + currScore);
+                removeCurrWordLetters();
+            } else {
+                ((Button) v).setTextColor(Color.RED);
+            }
             break;
         case R.id.wordgame_pause_button:
             if (isPaused) {
@@ -90,6 +119,19 @@ public class Game extends Activity implements OnClickListener {
         }
     }
 
+    private void removeCurrWordLetters() {
+        Iterator<String> iterator = this.currSelections.iterator();
+        while (iterator.hasNext()) {
+            String tempStrArr[] = iterator.next().split(",");
+            int i = Integer.parseInt(tempStrArr[0]);
+            int j = Integer.parseInt(tempStrArr[1]);
+            board[i][j] = '\u0000';
+        }
+        this.currSelections.clear();
+        BoardView boardView = (BoardView) findViewById(R.id.wordgame_board_view);
+        boardView.postInvalidate();
+    }
+
     private char[][] getInitialBoard(int diff) {
         board = new char[total_rows][total_cols];
         // for (int i = 0; i < total_rows; i++) {
@@ -102,11 +144,24 @@ public class Game extends Activity implements OnClickListener {
         board[3][4] = 'V';
         board[3][3] = 'A';
         board[1][4] = 'M';
+        
         board[1][3] = 'R';
         board[1][2] = 'A';
         board[5][1] = 'O';
         board[5][3] = 'E';
         board[6][2] = 'H';
+        
+        board[1][0] = 'P';
+        board[1][2] = 'L';
+        board[5][4] = 'O';
+        board[5][3] = 'E';
+        board[6][4] = 'U';
+        
+        board[0][3] = 'S';
+        board[4][2] = 'C';
+        board[3][1] = 'T';
+        board[3][2] = 'A';
+        board[1][1] = 'W';
         return board;
     }
 
@@ -120,9 +175,55 @@ public class Game extends Activity implements OnClickListener {
             currWord = currWordBtn.getText().toString();
             currWord += board[row][col];
             currWordBtn.setText(currWord);
-
             // store (r, c) in currSelections list
             currSelections.add(row + "," + col);
+            if (currWord.length() > 2 && isWordValid(currWord)) {
+                currWordBtn.setTextColor(Color.GREEN);
+                isCurrWordValid = true;
+            } else {
+                isCurrWordValid = false;
+                currWordBtn.setTextColor(getResources().getColor(R.color.board_letter));
+            }
         }
+    }
+    
+    protected boolean isWordValid(String ipWord) {
+        ipWord = ipWord.toLowerCase();
+        if (bloomFilter.contains(ipWord) && !wordList.contains(ipWord)) {
+            Log.d(TAG, ipWord + " is a valid word.");
+            //playValidWordBeep();
+            //addWord(ipWord);
+            return true;
+        } else {
+            Log.d(TAG, ipWord + " is an invalid word.");
+            return false;
+        }
+    }
+    
+    private void addWord(String ipWord) {
+        wordList.add(ipWord);
+        //renderWorList();
+    }
+    
+    private BloomFilter<String> loadBitsetFromFile(String filepath) {
+        try {
+            AssetManager am = this.getAssets();
+            int fileLength = (int) am.openFd(filepath).getLength();
+            Log.d(TAG, "compressed file length = " + fileLength);
+            InputStream is = am.open(filepath);
+
+            byte[] fileData = new byte[fileLength];
+            DataInputStream dis = new DataInputStream(is);
+            dis.readFully(fileData);
+            dis.close();
+            bloomFilter = new BloomFilter<String>(0.0001, 450000);
+            bloomFilter = BloomFilter.loadBitsetWithByteArray(fileData,
+                    bloomFilter);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bloomFilter;
     }
 }
